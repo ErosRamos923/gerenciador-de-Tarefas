698e0fa32bd5469497609c7d5d70dfab015e974a
ErosRamos923 <111581569+ErosRamos923@users.noreply.github.com>
2023-10-18 00:41:53 -0300

    Adicionar arquivos de projeto.

---
+++ gerenciador de Tarefas/Tarefas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gerenciador_de_Tarefas
+{
+    public class Tarefas
+    {
+
+    }
+}
